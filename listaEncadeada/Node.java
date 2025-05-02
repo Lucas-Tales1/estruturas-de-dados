4b1010de203d4a1000f9f16b508297668d69be64
Lucas-Tales1 <lucastales100@gmail.com>
2025-05-02 18:44:10 -0300

    implementação de listaEncadeada

---
+++ listaEncadeada/Node.java
@@ -0,0 +1,27 @@
+package listaEncadeada;
+
+public class Node {
+    private Object elemento;
+    private Node proximo;
+
+    public Node(Object elemento, Node proximo)  {
+        this.elemento = elemento;
+        this.proximo = proximo;
+    }
+
+    public Object getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Object elemento) {
+        this.elemento = elemento;
+    }
+
+    public Node getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(Node proximo) {
+        this.proximo = proximo;
+    }
+}

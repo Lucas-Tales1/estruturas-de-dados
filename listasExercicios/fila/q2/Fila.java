63f505c7ef5eb00b9c2aba98c3e83bc4a3253dcb
Lucas-Tales1 <lucastales100@gmail.com>
2025-11-11 17:09:41 -0300

    fix: remover pastas antigas

--- listasExercicios/fila/q2/Fila.java
+++
@@ -1,9 +0,0 @@
-package listasExercicios.fila.q2;
-
-public interface Fila {
-    public void enqueue(Object o);
-    public Object dequeue();
-    public int size();
-    public boolean isEmpty();
-}
-
--- listasExercicios/fila/q2/FilaVaziaExececao.java
+++
@@ -1,7 +0,0 @@
-package listasExercicios.fila.q2;
-
-public class FilaVaziaExececao extends RuntimeException {
-    public FilaVaziaExececao(String err){
-    super(err);
-    }
-}

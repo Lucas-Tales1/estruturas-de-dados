7789d53ead2e47bc01a66962a971b485ea5262cf
Lucas Tales <lucastales100@gmail.com>
2025-11-18 11:14:32 -0300

    Enhance Lista class with dynamic array and insert methods
    Refactor Lista class to include dynamic resizing and insert methods.

--- lista/implementacao/array/Lista.java
+++ lista/implementacao/array/Lista.java
@@ -1,5 +1,6 @@
 public class Lista implements ListaI{
     Object[] array;
+    int capacidade;
     int contador;
 
     public Lista(int capacidade) {
@@ -40,9 +41,54 @@ public class Lista implements ListaI{
         array[indice2] = temp;
     }
 
-    public insertBefore(int indice, Object o){
+    public Object insertBefore(int indice, Object o){
         if (size() == capacidade){
-            
+            Object[] novoArray = new Object[capacidade * 2];
+            for (int i = 0; i < capacidade; i++){
+                novoArray[i] = array[i];
+            }
+            array = novoArray;
+            capacidade *= 2;
         }
+
+        for (int i = indice ; i < size(); i++){
+            array[i+1] = array[i]; 
+        }
+
+        array[indice] = o;
+        contador++;
+
+        return o;
     }
+
+    public Object insertAfter(int indice, Object o){
+        insertBefore(indice + 1, o);
+        contador++;
+        return o;
+    }
+
+    public Object insertFirst(Object o){
+        insertBefore(0, o);
+        contador++;
+        return o;
+    }
+
+    public Object insertLast(Object o){
+        insertBefore(size(), o);
+        contador++;
+        return o;
+    }
+
+    public Object remove(int indice){
+        Object temp = array[indice];
+
+        for (int i = indice; i < size() - 1; i++){
+            array[i] = array[i + 1];
+        }
+
+        contador--;
+
+        return temp;
+    }
+
 }
